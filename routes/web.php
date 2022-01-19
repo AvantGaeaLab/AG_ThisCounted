e29ffd8609fb1275cbaaa853054c3f25eb71a9c9
Meshari Aljohani <mshari.aljuhani0@gmail.com>
2022-01-19 12:20:10 +0300

    prepare Migrations-Models-Controllers-Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DealController;
+use App\Http\Controllers\MerchantController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +24,24 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//Resource routes
+Route::resource('deals', DealController::class);
+Route::resource('merchants', MerchantController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('orders', OrderController::class);
+
+//Dashboard routes
+Route::get('/admin_dashboard', [App\Http\Controllers\DashboardController::class, 'admin_dashboards'])->name('admin_dashboards');
+Route::get('/user_dashboard', [App\Http\Controllers\DashboardController::class, 'user_dashboards'])->name('user_dashboards');
+
+//Favorite routes
+Route::post('favorite', [App\Http\Controllers\FavoriteController::class, 'store'])->name('favorite.store');
+Route::delete('favorite', [App\Http\Controllers\FavoriteController::class, 'destroy'])->name('favorite.destroy');
+Route::get('favorite', [App\Http\Controllers\FavoriteController::class, 'index'])->name('favorite.index');
+Route::get('favorite', [App\Http\Controllers\FavoriteController::class, 'show'])->name('favorite.show');
+
+//Pages Routes
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('', [App\Http\Controllers\PageController::class, 'index'])->name('home');
+
+
