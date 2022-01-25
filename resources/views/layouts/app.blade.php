2a8157263e739fbd4a1da7c344e92714f11aaec5
Meshari Aljohani <mshari.aljuhani0@gmail.com>
2022-01-25 14:42:57 +0300

    make responsive header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 
@@ -37,150 +36,25 @@
 
 </head>
 <body class="flex-wrapper myBg antialiased">
-<div id="app" class="myContainer0 ">
-
-    <!-- Header -->
-    <nav class="myNav navbar navbar-expand-md align-items-center ">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar navbar-expand-lg container-fluid">
-            <div class="col-xs-4 col-md-4">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img  src="{{asset('images/logo.png')}}" alt="ThisCounted Logo" width="70%" height="70%">
-                </a>
-            </div>
-            <div class="col mt-auto mb-auto">
-                <a href="https://t.me/ThisCounted" target="_blank" class="btn telegramBut MainButt"> join our channel</a>
-                <a href="https://www.instagram.com/thiscounted/" target="_blank" class="btn instagramBut MainButt" > Instagram </a>
-            </div>
-
-            <!-- Search bar -->
-            <div class="navbar-nav col">
-                <form class="form-group my-2" role="search">
-                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-warning MainButt my-2" type="submit">Search</button>
-                </form>
-            </div>
-            <!-- Authentication Links -->
-            <ul class="navbar-nav me-5">
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link  btn MainButt me-2" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link btn MainButt" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <a class="mt-2 me-2" href="{{route('favorite.show')}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="red"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
-                    </a>
-
-                    <li class="nav-item dropdown btn-group ">
-                        <a id="navbarDropdown" class="me-2 btn MainButt font-weight-bold nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu"  aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item  " href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                            <a class="dropdown-item  " href="{{ route('user_dashboard') }}">
-                                {{ __('User dashboard') }}
-                            </a>
-                            @if(Auth::id()<=3)
-                                <a class="dropdown-item  " href="{{ route('admin_dashboard') }}">
-                                    {{ __('Admin dashboard') }}
-                                </a>
-                                <a class="dropdown-item  " href="{{ route('deals.create') }}">
-                                    {{ __('Create new deal') }}
-                                </a>
-                            @endif
-                        </div>
-                    </li>
-
-            </ul>
-
-            @endguest
-        </div>
-    </nav>
-    <!-- END Header -->
-    <main class="myContainer container py-5">
-        @if($errors->any())
-            <div class="alert alert-warning">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        @yield('content')
-    </main>
-
-    <!-- FOOTER -->
-    <div class="myFooter">
-        <footer class="text-center text-start footer">
-            <!-- Grid container -->
-            <div class="container">
-                <!-- Newsletter Subs -->
-                <div class="container">
-                    <div class="row">
-                        <div class="col">
-                            <form action="">
-                                <!--Grid row-->
-                                <!--Grid column-->
-                                <div>
-                                    <p>
-                                        <strong>Sign up for our newsletter</strong>
-                                    </p>
-                                </div>
-                                <div>
-                                    <div>
-                                        <input type="email" class="form-control" placeholder="Email" />
-                                        <button type="submit" class="btn MainButt">Subscribe</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <!-- End Newsletter Subs -->
-
-                        <!-- Logo&Rights -->
-                        <div class="col">
-                            <div class="navbar-brand">
-                                <a  href="{{ url('/') }}">
-                                    <img  src="{{asset('images/logo.png')}}" alt="ThisCounted Logo" width="70%" height="70%">
-                                </a>
-                                <div>
-                                    <p>
-                                        <strong>2022 ThisCounted. All rights reserved.
-                                            <br>design by Avant Gaea
-                                        </strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- END Logo&Rights -->
-
-                        <div class="col mt-auto mb-auto">
-                            <img src="https://img.icons8.com/fluency/48/000000/telegram-app.png"/>
-                            <img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/>
-                            <img src="https://img.icons8.com/ios/50/000000/chat-message--v1.png"/>
-                        </div>
-                    </div>
+    <div id="app" class="myContainer0 ">
+        <!-- Header -->
+        @include('layouts.header')
+        <!-- END Header -->
+        <main class="myContainer container py-5">
+            @if($errors->any())
+                <div class="alert alert-warning">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
                 </div>
-            </div>
-        </footer>
+            @endif
+            @yield('content')
+        </main>
+        <!-- FOOTER -->
+        @include('layouts.footer')
     </div>
-</div>
-
 </body>
+
 </html>
