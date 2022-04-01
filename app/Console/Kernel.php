676c037365cd7f0c512ad5a8e87c63bc4107b4fc
Meshari Aljohani <mshari.aljuhani0@gmail.com>
2022-04-02 00:23:28 +0300

    The auto change for status for expired deals

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('expiresDeals')
+             ->everyMinute();
+    }
+
+    public function scheduleTimeZone(){
+     return 'Singapore';
     }
 
     /**
